03d4cfc0ff5a89a7a9cff45fa37125b8061dcc89
Gregory <gregorydebom@gmail.com>
2026-03-24 23:06:24 -0300

    feat: Criando a geração de código

---
+++ app/retirada.js
@@ -0,0 +1,40 @@
+import { View, Text, TouchableOpacity, StyleSheet, Button, Alert} from 'react-native';
+import { useRouter } from 'expo-router';
+import { useState } from 'react'; 
+
+export default function Sobre() {
+  const router = useRouter();
+  
+  const [armazenar, setArmazenar] = useState("");
+
+  function gerarNovoCodigo() {
+    const codigoAleatorio = Math.floor(Math.random() * 10000)
+      .toString()
+      .padStart(4, '0');
+    
+    setArmazenar(codigoAleatorio);
+    
+  }
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.titulo}>Retirada</Text>
+
+        <Text style={styles.descricao}>
+          Código gerado: {armazenar || "----"}
+        </Text>
+    
+        <TouchableOpacity style={styles.CorButton} onPress={gerarNovoCodigo}>
+           <Text style={styles.voltar}>Gerar código de retirada</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:  { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#212425' },
+  titulo:     { fontSize: 28, fontWeight: 'bold', marginBottom: 12, color: '#fff' },
+  descricao:  { fontSize: 18, color: '#fff', marginBottom: 24 },
+  voltar:     { fontSize: 16, color: '#E83D84', fontWeight: '600' },
+  CorButton:  { backgroundColor: '#121213', padding: 15, borderRadius: 8, alignItems: 'center', width: '80%' }
+});
